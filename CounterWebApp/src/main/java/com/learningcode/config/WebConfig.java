113536e7e73e54853f0d3d6eb5664ccaff59ac5c
lbuthman <lbuthman@gmail.com>
2017-07-09 17:23:31 -0500

    add web config to replace dispatcher xml

---
+++ CounterWebApp/src/main/java/com/learningcode/config/WebConfig.java
@@ -0,0 +1,37 @@
+package com.learningcode.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+/**
+ * Listing 5.2 A minimal yet useful configuration for Spring MVC
+ */
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.learningcode")
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setViewClass(JstlView.class);
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+
+        configurer.enable();
+    }
+
+}
