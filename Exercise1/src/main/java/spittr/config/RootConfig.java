085ed1f677dd966c691163b666fcd7e91912c669
lbuthman <lbuthman@gmail.com>
2017-07-11 06:03:20 -0500

    add root config placeholder for non-web application

---
+++ Exercise1/src/main/java/spittr/config/RootConfig.java
@@ -0,0 +1,19 @@
+package spittr.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+/**
+ * simple RootConfig for web
+ */
+
+@Configuration
+@ComponentScan(basePackages = {"spittr"},
+                excludeFilters = {@Filter(type=FilterType.ANNOTATION,
+                value=EnableWebMvc.class)})
+public class RootConfig {
+
+}
