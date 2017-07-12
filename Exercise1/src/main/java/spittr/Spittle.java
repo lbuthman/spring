2a770546eb89d37132d51bf909c39df2eab1491d
lbuthman <lbuthman@gmail.com>
2017-07-12 12:07:12 -0500

    correct constructors to handle id parameter

--- Exercise1/src/main/java/spittr/Spittle.java
+++ Exercise1/src/main/java/spittr/Spittle.java
@@ -14,19 +14,19 @@ public class Spittle {
     private final Long id;
     private final String message;
     private final Date time;
-    private Double latitude;
     private Double longitude;
+    private Double latitude;
 
     public Spittle(String message, Date time) {
-        this(message, time, null, null);
+        this(null, message, time, null, null);
     }
 
-    public Spittle(String message, Date time, Double latitude, Double longitude) {
-        this.id = null;
+    public Spittle(Long id, String message, Date time, Double longitude, Double latitude) {
+        this.id = id;
         this.message = message;
         this.time = time;
-        this.latitude = latitude;
         this.longitude = longitude;
+        this.latitude = latitude;
     }
 
     public Long getId() {
@@ -41,12 +41,12 @@ public class Spittle {
         return time;
     }
 
-    public Double getLatitude() {
-        return latitude;
-    }
-
     public Double getLongitude() {
         return longitude;
+    }
+
+    public Double getLatitude() {
+        return latitude;
     }
 
     @Override
