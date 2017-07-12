2f5858769114c16859528537d8b05bd4c50af739
lbuthman <lbuthman@gmail.com>
2017-07-12 05:26:03 -0500

    add component annotation for autowiring

--- Exercise1/src/main/java/spittr/data/SpittleRepository.java
+++ Exercise1/src/main/java/spittr/data/SpittleRepository.java
@@ -1,12 +1,14 @@
 package spittr.data;
 
 import java.util.List;
+
+import org.springframework.stereotype.Component;
 import spittr.Spittle;
 
 /**
  * Simple interface to set stage for database integration
  */
-
-public interface SpittleRespository {
+@Component
+public interface SpittleRepository {
     List<Spittle> findSpittles(long max, int count);
 }
