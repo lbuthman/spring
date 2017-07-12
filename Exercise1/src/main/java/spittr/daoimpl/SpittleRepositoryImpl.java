f9284baaf31d88f33e95b9f51865ff598e4146b1
lbuthman <lbuthman@gmail.com>
2017-07-12 18:51:31 -0500

    create implementation of interface so that it runs

---
+++ Exercise1/src/main/java/spittr/daoimpl/SpittleRepositoryImpl.java
@@ -0,0 +1,29 @@
+package spittr.daoimpl;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Date;
+
+import org.springframework.stereotype.Component;
+
+import spittr.data.SpittleRepository;
+import spittr.Spittle;
+
+/**
+ * in the hope of actually running
+ */
+@Component
+public class SpittleRepositoryImpl implements SpittleRepository {
+
+    @Override
+    public List<Spittle> findSpittles(long max, int count) {
+
+        List<Spittle> spittles = new ArrayList<Spittle>();
+        for (int i = 0; i < count; i++) {
+            spittles.add(new Spittle("Spittle" + i, new Date()));
+        }
+        return spittles;
+
+    }
+
+}
