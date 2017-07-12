df580f185a132b3e901f1a1f429cea0be6fb168b
lbuthman <lbuthman@gmail.com>
2017-07-12 06:17:47 -0500

    add abandoned test, Mockito dependency not working with Maven

---
+++ Exercise1/src/test/java/web/SpittleControllerTest.java
@@ -0,0 +1,41 @@
+/*
+package web;
+
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Date;
+
+import org.junit.Test;
+
+import spittr.Spittle;
+import spittr.web.SpittleController;
+import spittr.data.SpittleRepository;
+
+*/
+/**
+ * Listing 5.9 Testing that SpittleController handles GET requests for /spittles
+ *//*
+
+
+public class SpittleControllerTest {
+
+    @Test
+    public void shouldShowRecentSpittles() throws Exception {
+        List<Spittle> expectedSpittles = createSpittleList(20);
+        //SpittleRepository mockRepository = mock(SpittleRepository.class);
+    }
+
+    private List<Spittle> createSpittleList(int count) {
+        List<Spittle> spittles = new ArrayList<>();
+        for (int i=0; i<count; i++) {
+            spittles.add(new Spittle("Spittle " + i, new Date()));
+        }
+        return spittles;
+    }
+}
+*/
