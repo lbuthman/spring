b0fda6dff51979f93eb663ece484161376ef53c7
lbuthman <lbuthman@gmail.com>
2017-07-11 19:46:08 -0500

    refactor package for controller

--- Exercise1/src/main/java/spittr/web/HomeController.java
+++ Exercise1/src/main/java/spittr/web/HomeController.java
@@ -1,9 +1,8 @@
-package spittr.controller;
+package spittr.web;
 
 import static org.springframework.web.bind.annotation.RequestMethod.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Listing 5.3 HomeController: an example of an extremely simple controller
