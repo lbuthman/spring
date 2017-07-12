fe953f7172d9d9218d7cdba9ecaa6b0e29369cc4
lbuthman <lbuthman@gmail.com>
2017-07-11 20:09:24 -0500

    refactor package to web for consistency

--- Exercise1/src/test/java/web/HomeControllerTest.java
+++ Exercise1/src/test/java/web/HomeControllerTest.java
@@ -1,4 +1,4 @@
-package Controllers;
+package web;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
