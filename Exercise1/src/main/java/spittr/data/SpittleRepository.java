da72be828aba118435abdcb71f7bcbb925e50ef3
lbuthman <lbuthman@gmail.com>
2017-07-12 18:50:56 -0500

    removed component annotation

--- Exercise1/src/main/java/spittr/data/SpittleRepository.java
+++ Exercise1/src/main/java/spittr/data/SpittleRepository.java
@@ -2,13 +2,12 @@ package spittr.data;
 
 import java.util.List;
 
-import org.springframework.stereotype.Component;
 import spittr.Spittle;
 
 /**
  * Simple interface to set stage for database integration
  */
-@Component
+
 public interface SpittleRepository {
     List<Spittle> findSpittles(long max, int count);
 }
