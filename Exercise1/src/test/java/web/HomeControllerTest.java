d3c706be6c8b351f058959322e1305735f6a3b28
lbuthman <lbuthman@gmail.com>
2017-07-12 06:18:01 -0500

    refactor import statement ordering

--- Exercise1/src/test/java/web/HomeControllerTest.java
+++ Exercise1/src/test/java/web/HomeControllerTest.java
@@ -3,8 +3,8 @@ package web;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
-
 import org.springframework.test.web.servlet.MockMvc;
+
 import org.junit.Test;
 import spittr.web.HomeController;
 
