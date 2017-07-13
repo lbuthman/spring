d180162c528fefc833b2fbbc4f40932133c8921d
lbuthman <lbuthman@gmail.com>
2017-07-13 18:14:00 -0500

    refactor spittles and add spittle to take request params

--- Exercise1/src/main/java/spittr/web/SpittleController.java
+++ Exercise1/src/main/java/spittr/web/SpittleController.java
@@ -1,12 +1,17 @@
 package spittr.web;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.ui.Model;
 
+import org.springframework.web.bind.annotation.RequestParam;
 import spittr.data.SpittleRepository;
+import spittr.Spittle;
 
 /**
  * Listing 5.10 SpittleController: places a list of recent spittles in the model
@@ -24,9 +29,17 @@ public class SpittleController {
         this.spittleRepository = spittleRepository;
     }
 
-    @RequestMapping(method = RequestMethod.GET)
-    public String spittles(Model model) {
-        model.addAttribute(spittleRepository.findSpittles(Long.MAX_VALUE, 20));
+    @RequestMapping(value="/{spittleId}", method=RequestMethod.GET)
+    public String spittle(@PathVariable("spittleId") long spittleId, Model model) {
+        model.addAttribute(spittleRepository.findOne(spittleId));
         return "spittles";
+    }
+
+    @RequestMapping(method=RequestMethod.GET)
+    public List<Spittle> spittles(
+            @RequestParam(value="max", defaultValue="9223372036854775807") long max,
+            @RequestParam(value="count", defaultValue="20") int count) {
+
+        return spittleRepository.findSpittles(max, count);
     }
 }
