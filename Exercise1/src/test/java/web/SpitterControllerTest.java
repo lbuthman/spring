dae135f08af1b989a1f9f7fbdb7639b707ccb359
lbuthman <lbuthman@gmail.com>
2017-07-13 21:02:23 -0500

    add test for post method

--- Exercise1/src/test/java/web/SpitterControllerTest.java
+++ Exercise1/src/test/java/web/SpitterControllerTest.java
@@ -27,10 +27,34 @@ public class SpitterControllerTest {
 
     @Test
     public void shouldShowRegistration() throws Exception {
-        SpitterController controller = new SpitterController();
+        SpitterRepository mockRepository = mock(SpitterRepository.class);
+        SpitterController controller = new SpitterController(mockRepository);
         MockMvc mockMvc = standaloneSetup(controller).build();
 
         mockMvc.perform(get("/spitter/register"))
                 .andExpect(view().name("registerForm"));
+    }
+
+    @Test
+    public void shouldProcessRegistration() throws Exception {
+        SpitterRepository mockRepository = mock(SpitterRepository.class);
+
+        Spitter unsaved = new Spitter("jbauer", "24hours",
+                "Jack", "Bauer", "jbauer@gmail.com");
+        Spitter saved = new Spitter(24L, "jbauer", "24hours",
+                "Jack", "Bauer", "jbauer@gmail.com");
+        when(mockRepository.save(unsaved)).thenReturn(saved);
+
+        SpitterController controller = new SpitterController(mockRepository);
+        MockMvc mockMvc = standaloneSetup(controller).build();
+
+        mockMvc.perform(post("/spitter/register")
+                .param("firstName", "Jack")
+                .param("lastName", "Bauer")
+                .param("username", "jbauer")
+                .param("password", "24hours"))
+                .andExpect(redirectedUrl("/spitter/jbauer"));
+
+        verify(mockRepository, atLeastOnce()).save(unsaved);
     }
 }
