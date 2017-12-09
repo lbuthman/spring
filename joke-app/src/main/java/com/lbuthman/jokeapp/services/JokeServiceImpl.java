1d8c69d45e62b1f366f86f3f50851f9cb9914c12
Luke Buthman <lbuthman@gmail.com>
2017-12-08 18:13:09 -0600

    [fix] Ensure only one ChuckNorrisQuotes object is made

--- joke-app/src/main/java/com/lbuthman/jokeapp/services/JokeServiceImpl.java
+++ joke-app/src/main/java/com/lbuthman/jokeapp/services/JokeServiceImpl.java
@@ -5,9 +5,15 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class JokeServiceImpl implements JokeService {
+
+    private final ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public JokeServiceImpl() {
+        chuckNorrisQuotes = new ChuckNorrisQuotes();
+    }
+
     @Override
     public String getJoke() {
-        ChuckNorrisQuotes quotes = new ChuckNorrisQuotes();
-        return quotes.getRandomQuote();
+        return chuckNorrisQuotes.getRandomQuote();
     }
 }
