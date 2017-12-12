d4cfb44476e1e8a0a754b38a4f6679acfa85118a
Luke Buthman <lbuthman@gmail.com>
2017-12-12 12:15:28 -0600

    [feat] Add initial db values

---
+++ recipes/src/main/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO unit_of_measure (description) VALUES ('teaspoon');
+INSERT INTO unit_of_measure (description) VALUES ('Tablespoon');
+INSERT INTO unit_of_measure (description) VALUES ('Dash');
+INSERT INTO unit_of_measure (description) VALUES ('Cup');
+INSERT INTO unit_of_measure (description) VALUES ('Pint');
+
+INSERT INTO category (description) VALUES ('American');
+INSERT INTO category (description) VALUES ('Mexican');
+INSERT INTO category (description) VALUES ('Italian');
+INSERT INTO category (description) VALUES ('French');
+INSERT INTO category (description) VALUES ('Chinese');
