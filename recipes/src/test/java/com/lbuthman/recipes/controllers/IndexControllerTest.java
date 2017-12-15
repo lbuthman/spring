c4b1a084e83020606db939b094fe19bcb6b91faa
Luke Buthman <lbuthman@gmail.com>
2017-12-15 05:02:05 -0600

    [test] Test get method with mockMvc

--- recipes/src/test/java/com/lbuthman/recipes/controllers/IndexControllerTest.java
+++ recipes/src/test/java/com/lbuthman/recipes/controllers/IndexControllerTest.java
@@ -7,19 +7,23 @@ import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertSame;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 public class IndexControllerTest {
 
-    private IndexController indexController;
+    private IndexController controller;
 
     @Mock
     private RecipeService recipeService;
@@ -30,11 +34,19 @@ public class IndexControllerTest {
     @Before
     public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
-        indexController = new IndexController(recipeService);
+        controller = new IndexController(recipeService);
     }
 
     @Test
-    public void getIndex() {
+    public void getIndex() throws Exception {
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"));
+    }
+
+    @Test
+    public void getIndexModel() {
 
         //given
         List<Recipe> recipes = new ArrayList<>();
@@ -46,10 +58,9 @@ public class IndexControllerTest {
         ArgumentCaptor<List<Recipe>> argumentCaptor = ArgumentCaptor.forClass(List.class);
 
         //when
-        String viewName = indexController.getIndex(model);
+        String viewName = controller.getIndex(model);
 
         //then
-        assertSame("index", viewName);
         verify(recipeService, times(1)).getRecipes();
         verify(model, times(1)).addAttribute(eq("recipes"), argumentCaptor.capture());
         List<Recipe> listFromController = argumentCaptor.getValue();
