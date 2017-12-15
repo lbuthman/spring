550dc9942fdcaeb1b806b5b0d4aa9e0addbc8009
Luke Buthman <lbuthman@gmail.com>
2017-12-14 19:17:44 -0600

    [refactor:test] Add ArgumentCapture for more accurate test

--- recipes/src/test/java/com/lbuthman/recipes/controllers/IndexControllerTest.java
+++ recipes/src/test/java/com/lbuthman/recipes/controllers/IndexControllerTest.java
@@ -1,17 +1,21 @@
 package com.lbuthman.recipes.controllers;
 
+import com.lbuthman.recipes.domain.Recipe;
 import com.lbuthman.recipes.services.RecipeService;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertSame;
-import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 public class IndexControllerTest {
 
@@ -31,10 +35,24 @@ public class IndexControllerTest {
 
     @Test
     public void getIndex() {
+
+        //given
+        List<Recipe> recipes = new ArrayList<>();
+        recipes.add(new Recipe());
+        recipes.add(new Recipe());
+
+        when(recipeService.getRecipes()).thenReturn(recipes);
+
+        ArgumentCaptor<List<Recipe>> argumentCaptor = ArgumentCaptor.forClass(List.class);
+
+        //when
         String viewName = indexController.getIndex(model);
 
+        //then
         assertSame("index", viewName);
         verify(recipeService, times(1)).getRecipes();
-        verify(model, times(1)).addAttribute(eq("recipes"), anyList());
+        verify(model, times(1)).addAttribute(eq("recipes"), argumentCaptor.capture());
+        List<Recipe> listFromController = argumentCaptor.getValue();
+        assertEquals(2, listFromController.size());
     }
 }
