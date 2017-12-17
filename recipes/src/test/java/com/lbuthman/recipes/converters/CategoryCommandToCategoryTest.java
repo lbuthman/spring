ca44afc1f67376be381c5a5430e3fdb36aca54f5
Luke Buthman <lbuthman@gmail.com>
2017-12-16 19:30:12 -0600

    [test] Test converter functionality

--- recipes/src/test/java/com/lbuthman/recipes/converters/CategoryCommandToCategoryTest.java
+++ recipes/src/test/java/com/lbuthman/recipes/converters/CategoryCommandToCategoryTest.java
@@ -1,5 +1,35 @@
-import static org.junit.Assert.*;
+package com.lbuthman.recipes.converters;
+
+import com.lbuthman.recipes.commands.CategoryCommand;
+import com.lbuthman.recipes.domain.Category;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class CategoryCommandToCategoryTest {
 
+    private static final Long ID_VALUE = new Long(1);
+    private static final String DESCRIPTION = "description";
+    CategoryCommandToCategory converter;
+
+    @Before
+    public void setUp() throws Exception {
+        converter = new CategoryCommandToCategory();
+    }
+
+    @Test
+    public void convert() {
+        //given
+        CategoryCommand categoryCommand = new CategoryCommand();
+        categoryCommand.setId(ID_VALUE);
+        categoryCommand.setDescription(DESCRIPTION);
+
+        //when
+        Category category = converter.convert(categoryCommand);
+
+        //then
+        assertEquals(ID_VALUE, category.getId());
+        assertEquals(DESCRIPTION, category.getDescription());
+    }
 }
